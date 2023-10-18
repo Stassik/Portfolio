a15fb133ac5f608e9676a8971c842e02986269c8
unknown <nastase4ka@mail.ru>
2023-10-18 23:34:36 +0300

    modified project

---
+++ Alex_land/script/script.js
@@ -0,0 +1,34 @@
+
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    breakpoints: {
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+
+
+
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});

daa527f211f7e1d7c642712686c68ea257058a23
unknown <nastase4ka@mail.ru>
2023-11-01 00:52:52 +0300

    adaptive burger menu added

--- Alex_land/script/script.js
+++ Alex_land/script/script.js
@@ -38,8 +38,45 @@ var swiper = new Swiper(".swiper", {
 });
 
 
+// Клонирование элементов для бургер меню
+const burgerMenu = document.getElementById('burgerNav');
+
+const contacts = document.querySelector('.contacts-city');
+const contactsClone = contacts.cloneNode(true);
+
+const logo = document.querySelector('.header__logo');
+const logoClone = logo.cloneNode(true);
+
+const burgerList = document.querySelector('.header__nav-list');
+const burgerListClone = burgerList.cloneNode(true);
+
+const contactsPhone = document.querySelector('.contacts-phone');
+const contactsPhoneClone = contactsPhone.cloneNode(true);
+
+const contactsSocial = document.querySelector('.contacts-social');
+const contactsSocialClone = contactsSocial .cloneNode(true);
+
+const burgerBtn = document.querySelector('.header__btn.btn_light');
+const burgerBtnClone = burgerBtn .cloneNode(true);
+
+burgerMenu.innerHTML = '<div class="burger-nav__header"><svg class="close__icon" id="close"><use xlink:href="img/sprite.svg#close"></use></svg></div>';
+burgerMenu.querySelector('.burger-nav__header').prepend(logoClone);
+
+burgerMenu.appendChild(contactsClone);
+
+burgerMenu.querySelector('.contacts-city__label').setAttribute('for', 'myCityBurger');
+burgerMenu.querySelector('#myCity').setAttribute('id', 'myCityBurger');
+burgerMenu.appendChild(burgerList);
+burgerMenu.appendChild(contactsPhoneClone);
+burgerMenu.appendChild(contactsSocialClone);
+burgerMenu.appendChild(burgerBtnClone);
+
+
+
 /**Закрытие бургер меню */
 const close = document.getElementById('close');
-close.addEventListener('click', function() {
-   document.getElementById('burger').checked = false;
+close.addEventListener('click', function () {
+    document.getElementById('burger').checked = false;
 });
+
+
